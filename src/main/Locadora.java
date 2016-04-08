836d944159d5cf01d7bae628bc5f45d9ab5aef49
darlanbo <darlanboelter@gmail.com>
2016-04-07 22:26:03 -0300

    alteracoes de menu

--- src/main/Locadora.java
+++ src/main/Locadora.java
@@ -12,11 +12,81 @@ public class Locadora {
 
 	public static void main(String[] args) {
 
-		/*cadastraVeiculo();
-		cadastraCliente();
-		efetuaLocacao();*/
-		mostraMenu();
- }
+		int op=0;
+
+		do{
+			op = mostraMenu();
+			switch (op)
+			{
+			case 1:
+				int opCliente = 0;
+
+				do{
+					opCliente = menuCliente();
+					switch(opCliente){
+					case 1:
+						cadastraCliente();
+						break;
+					case 2:
+						buscaCliente();
+						break;
+					default:
+						System.out.println("Opção inválida!");
+						break;
+					case 99:
+						break;
+					}
+				}while(opCliente != 99);
+				break;
+					
+			case 2:
+				int opVeiculo = 0;
+				
+				do{
+					opVeiculo = menuVeiculos();
+					switch(opVeiculo){
+					case 1:
+						cadastraVeiculo();
+						break;
+					case 2:
+						buscaVeiculo();
+						break;
+					default:
+						System.out.println("Opção inválida!");
+						break;
+					}
+				}while(opVeiculo != 99);
+				break;
+				
+			case 3: 
+				int opLocacoes = 0;
+				
+				do{
+					opLocacoes = menuLocacoes();
+					switch(opLocacoes){
+					case 1:
+						efetuaLocacao();
+						break;
+					case 2: 
+						System.out.println("OP2 Locacoes");
+						break;
+					default:
+						System.out.println("Opção inválida!");
+						break;
+					}
+				}while(opLocacoes != 99);
+				break;
+				
+			case 99:
+				System.exit(0);
+			default:
+				System.out.println("Opção inválida!");
+				break;
+			}
+
+		}while(op != 99);
+
+	}
 
 	public static Cliente buscaCliente(){
 		System.out.println("Informe o seu RG para pesquisa: ");
@@ -29,23 +99,23 @@ public class Locadora {
 		}
 		return null;
 	}
-		
+
 	public static Veiculo buscaVeiculo(){
-			System.out.println("Informe a placa do veiculo para pesquisa: ");
-			String busca = sc.next();
-			for(Veiculo vei: cadastroVeiculos){
-				if(busca.equalsIgnoreCase(vei.getPlaca())){
-					System.out.println(" Encontrei o veiculo que voce procurava! ");
-					return vei;
-				}
+		System.out.println("Informe a placa do veiculo para pesquisa: ");
+		String busca = sc.next();
+		for(Veiculo vei: cadastroVeiculos){
+			if(busca.equalsIgnoreCase(vei.getPlaca())){
+				System.out.println(" Encontrei o veiculo que voce procurava! ");
+				return vei;
 			}
+		}
 		return null;	
 	}
-	
+
 	public static void cadastraVeiculo(){
-		
+
 		Veiculo vei = new Veiculo();
-		
+
 		System.out.println("##### INFORMACOES DO VEICULO #####");
 		System.out.println(" Informe a placa: ");
 		vei.setPlaca(sc.next());
@@ -57,13 +127,13 @@ public class Locadora {
 		vei.setAno(sc.nextInt());
 
 		cadastroVeiculos.add(vei);
-		
+
 	}
-	
+
 	public static void cadastraCliente(){
-		
+
 		Cliente cli = new Cliente();
-		
+
 		System.out.println("##### BEM VINDO AO SISTEMA DE LOCACAO DE VEICULOS #####");
 		System.out.println(" Informe seu nome para cadastro: ");
 		cli.setNome(sc.next());
@@ -74,23 +144,23 @@ public class Locadora {
 
 		cadastroClientes.add(cli);
 	}
-	
+
 	public static void efetuaLocacao(){
-		
+
 		Locacao lol = new Locacao();
-		
+
 		System.out.println("##### INFORMACOES DA LOCACAO DE VEICULO #####");
 
 		System.out.println(" Buscando cliente... ");
 		Cliente cli = buscaCliente();
-		
+
 		if(cli == null){
 			System.out.println("Cliente nao encontrado!");
 		}
 
 		System.out.println(" Buscando Veiculo... ");
 		Veiculo v = buscaVeiculo();
-		
+
 		if(v == null){
 			System.out.println("Veiculo nao encontrado!");
 		}
@@ -110,32 +180,66 @@ public class Locadora {
 
 		lol.setVeiculo(v);
 		lol.setCliente(cli);
-		
+
 		cadastroLocacao.add(lol);
-		
+
 		lol.mostraLocacao();		
 	}
 
-	public static void mostraMenu(){
-		
+	public static int mostraMenu(){
+
 		System.out.println("#### MENU ####");
-		System.out.println("1- Cadastro");
-		System.out.println("2- Pesquisa");
-		
-		System.out.println("Escolha uma das opcoes e aperte ENTER: ");
+		System.out.println("Escolha uma das opcoes e aperte ENTER (99 para sair): ");
+
+		System.out.println("1- Cadastro de Clientes");
+		System.out.println("2- Cadastro de Veiculos");
+		System.out.println("3- Locacoes");
+
 		int op = sc.nextInt();
-		
-		System.out.println("Voce escolheu a opcao " + op );
-		
-		if(op == 1 ){
-			System.out.println("** Cadastro **");
-			 cadastraCliente();
-		}
-		
-		if(op == 2 ){
-			System.out.println("** Pesquisa **");
-			
-		}
-		
+
+		return op;
+
 	}
+
+	public static int menuCliente(){
+
+		System.out.println("#### MENU DO CLIENTE ####");
+		System.out.println("Escolha uma das opcoes e aperte ENTER: ");
+
+		System.out.println("1- Cadastro Cliente");
+		System.out.println("2- Buscar Cliente ");
+
+		int op = sc.nextInt();
+
+		return op;
+	}
+
+	public static int menuVeiculos(){
+
+		System.out.println("#### MENU DE VEICULOS ####");
+		System.out.println("Escolha uma das opcoes e aperte ENTER: ");
+
+		System.out.println("1- Cadastrar Veiculo");
+		System.out.println("2- Buscar Veiculo");
+
+		int op = sc.nextInt();
+
+		return op;
+	}
+
+	public static int menuLocacoes(){
+
+		System.out.println("#### MENU DE LOCACOES ####");
+		System.out.println("Escolha uma das opcoes e aperte ENTER: ");
+
+		System.out.println("1- Cadastro Locacoes");
+		System.out.println("2- Buscar Locacoes");
+
+
+		int op = sc.nextInt();
+
+		return op;
+	}
+
+
 }
